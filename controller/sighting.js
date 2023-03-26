bcb8360d5c2aefb206a89bb915b9759b233f440f
katefurtney <katefurtney@gmail.com>
2023-03-26 17:16:52 -0400

    table updates

--- controller/sighting.js
+++ controller/sighting.js
@@ -1,7 +1,7 @@
 const db = require('../pg_client')
 
 async function getAll(request, response) {
-    const result = await db.query("SELECT birds.species, sightings.year, sightings.country FROM birds INNER JOIN sightings ON birds.code = sightings.birds_code")
+    const result = await db.query("SELECT birds.species, sightings.day, sightings.month, sightings.year, sightings.city, sightings.state, sightings.country FROM birds INNER JOIN sightings ON birds.code = sightings.birds_code")
     response.send(result.rows)
 }
 
--- public/bird_data.js
+++ public/bird_data.js
@@ -8,18 +8,50 @@ fetch('/api/sightings')
         //add headers to result
         let result = `<tr>
                         <th>Species</th>
-                        <th>First Seen</th>
-                        <th>Country</th>
+                        <th>Date First Seen</th>
+                        <th>Location</th>
                     </tr>`;
 
         data.forEach(row => {
             // result += JSON.stringify(row)
             result += `<tr>`;
             result += `<td>${row.species}</td>`;
-            result += `<td class="table__year">${row.year || 'pre 2019'}</td>`;
-            result += `<td>${row.country}</td>`;
+            // result += `<td class="table__year">${row.year || 'pre 2019'}</td>`;
+            if (row.year === null) {
+                result += `<td class="table__year">${'pre 2019'}</td>`;
+            }
+            else if (row.month === null) {
+                result += `<td class="table__year">${row.year}</td>`;
+            }
+            else if (row.day === null) {
+                result += `<td class="table__year">${getMonthName(row.month) + ' ' + row.year}</td>`;
+            }
+            else {
+                result += `<td class="table__year">${getMonthName(row.month) + ' ' + row.day + ', ' + row.year /*|| 'pre 2019'*/}</td>`;
+            }
+            // result += `<td>${row.country}</td>`;
+            if (row.country === "") {
+                result += `td class="table__year">${'Unknown'}</td>`;
+            }
+            else if (row.state === "") {
+                result += `<td class="table__year">${row.country}</td>`;
+            }
+            else if (row.city === "") {
+                result += `<td class="table__year">${row.state + ', ' + row.country}</td>`;
+            }
+            else {
+                result += `<td class="table__year">${row.city + ', ' + row.state + ', ' + row.country}`
+            }
+
             result += `</tr>`;
         })
 
         table_element.innerHTML = result;
     })
+
+    function getMonthName(monthNumber) {
+        const date = new Date();
+        date.setMonth(monthNumber - 1);
+      
+        return date.toLocaleString('en-US', { month: 'long' });
+      }
