c71edf1158b1826673a014953639290641371131
katefurtney <katefurtney@gmail.com>
2023-06-24 14:26:42 -0400

    update postgres connection details & exports

--- pg_client.js
+++ pg_client.js
@@ -1,13 +1,24 @@
 const { Client } = require('pg')
 
 async function query(query) {
-    const client = new Client({
+    let connectionDetails = {
         host: 'localhost',
         port: '55000',
         user: 'postgres',
         password: 'postgrespw',
         database: 'postgres'
-    })
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        connectionDetails = {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    }
+
+    const client = new Client(connectionDetails);
 
     await client.connect()
     const result = await client.query(query)
